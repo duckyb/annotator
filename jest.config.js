43a186f1522009b76ed6332cbd0412f8957ae028
Duccio Breschi <breschi@netseven.it>
2025-05-23 10:53:27 +0200

    Update jest config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+};
